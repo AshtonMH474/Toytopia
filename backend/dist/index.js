1a58dfb863ff093edb02fd19cd1bf87bb7c1fbf3
Ashton <ashtonhowa@gmail.com>
2024-12-16 19:27:14 -0500

    connected db

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,8 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+require('dotenv').config();
+const config_1 = require("./config");
+const models_1 = __importDefault(require("./models"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+models_1.default.sequelize
+    .authenticate()
+    .then(() => {
+    console.log('Database connection success!');
+    return models_1.default.sequelize.sync(); // Synchronize the schema
+})
+    .then(() => {
+    console.log('Database schema synchronized!');
+    // Start listening for connections
+    app.listen(config_1.port, () => {
+        console.log(`Application running on port ${config_1.port}`);
+    });
+})
+    .catch((err) => {
+    console.error('Error during database initialization:', err);
+});
 app.get('/:id', (req, res) => {
     res.send('TEst!');
 });
@@ -13,8 +32,4 @@ app.post('/', (req, res) => {
     res.send({
         data: req.body
     });
-});
-const port = 8001;
-app.listen(port, () => {
-    console.log(`Application running on port ${port}`);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,32 @@
 import express from 'express'
 import { Request,Response } from 'express';
+require('dotenv').config();
+import {port} from './config'
+import db from './models';
+
 const app = express();
 
 app.use(express.json())
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection success!');
+
+    return db.sequelize.sync(); // Synchronize the schema
+  })
+  .then(() => {
+    console.log('Database schema synchronized!');
+
+    // Start listening for connections
+    app.listen(port, () => {
+      console.log(`Application running on port ${port}`);
+    });
+  })
+  .catch((err:any) => {
+    console.error('Error during database initialization:', err);
+  });
+
 
 app.get('/:id', (req:Request,res:Response) => {
     res.send('TEst!')
@@ -12,10 +36,4 @@ app.post('/',(req:Request,res:Response) => {
     res.send({
         data:req.body
     })
-})
-
-const port:number = 8001
-
-app.listen(port,() => {
-    console.log(`Application running on port ${port}`)
 })
