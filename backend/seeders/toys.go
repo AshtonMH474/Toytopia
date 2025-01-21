83796a654618386dda60ac7a78bc04571882c672
Ashton <ashtonhowa@gmail.com>
2025-01-20 22:55:59 -0500

    fixing seed data

--- backend/seeders/toys.go
+++ backend/seeders/toys.go
@@ -10,14 +10,14 @@ import (
 
 func SeedToys() {
 	toys := []models.Toy{
-		{ReleaseDate: time.Date(1984, time.January, 18, 0, 0, 0, 0, time.UTC), Price: 1000, ProductType: "Optimus Prime g1 Orginial", Theme: "Transformers", Count: 2, Available: true},
-		{ReleaseDate: time.Date(1995, time.December, 15, 0, 0, 0, 0, time.UTC), Price: 250.75, ProductType: "Buzz Lightyear Action Figure", Theme: "Toy Story", Count: 5, Available: true},
-		{ReleaseDate: time.Date(2001, time.November, 3, 0, 0, 0, 0, time.UTC), Price: 500, ProductType: "Millennium Falcon", Theme: "Star Wars", Count: 1, Available: true},
-		{ReleaseDate: time.Date(1989, time.May, 22, 0, 0, 0, 0, time.UTC), Price: 75, ProductType: "Michelangelo Action Figure", Theme: "Teenage Mutant Ninja Turtles", Count: 12, Available: true},
-		{ReleaseDate: time.Date(2010, time.March, 15, 0, 0, 0, 0, time.UTC), Price: 20.99, ProductType: "Lightning McQueen Diecast", Theme: "Cars", Count: 30, Available: true},
-		{ReleaseDate: time.Date(1978, time.October, 17, 0, 0, 0, 0, time.UTC), Price: 1000, ProductType: "Luke Skywalker Original Action Figure", Theme: "Star Wars", Count: 3, Available: true},
-		{ReleaseDate: time.Date(1998, time.June, 10, 0, 0, 0, 0, time.UTC), Price: 150, ProductType: "Red Ranger Figurine", Theme: "Power Rangers", Count: 8, Available: true},
-		{ReleaseDate: time.Date(2020, time.February, 1, 0, 0, 0, 0, time.UTC), Price: 35.5, ProductType: "Baby Yoda Plush", Theme: "The Mandalorian", Count: 50, Available: true},
+		{ReleaseDate: time.Date(1984, time.January, 18, 0, 0, 0, 0, time.UTC), Price: 1000, ProductType: "Optimus Prime g1 Orginial", Theme: "Transformers", Count: 2, Available: true, UserId: 1},
+		{ReleaseDate: time.Date(1995, time.December, 15, 0, 0, 0, 0, time.UTC), Price: 250.75, ProductType: "Buzz Lightyear Action Figure", Theme: "Toy Story", Count: 5, Available: true, UserId: 1},
+		{ReleaseDate: time.Date(2001, time.November, 3, 0, 0, 0, 0, time.UTC), Price: 500, ProductType: "Millennium Falcon", Theme: "Star Wars", Count: 1, Available: true, UserId: 1},
+		{ReleaseDate: time.Date(1989, time.May, 22, 0, 0, 0, 0, time.UTC), Price: 75, ProductType: "Michelangelo Action Figure", Theme: "Teenage Mutant Ninja Turtles", Count: 12, Available: true, UserId: 1},
+		{ReleaseDate: time.Date(2010, time.March, 15, 0, 0, 0, 0, time.UTC), Price: 20.99, ProductType: "Lightning McQueen Diecast", Theme: "Cars", Count: 30, Available: true, UserId: 1},
+		{ReleaseDate: time.Date(1978, time.October, 17, 0, 0, 0, 0, time.UTC), Price: 1000, ProductType: "Luke Skywalker Original Action Figure", Theme: "Star Wars", Count: 3, Available: true, UserId: 3},
+		{ReleaseDate: time.Date(1998, time.June, 10, 0, 0, 0, 0, time.UTC), Price: 150, ProductType: "Red Ranger Figurine", Theme: "Power Rangers", Count: 8, Available: true, UserId: 2},
+		{ReleaseDate: time.Date(2020, time.February, 1, 0, 0, 0, 0, time.UTC), Price: 35.5, ProductType: "Baby Yoda Plush", Theme: "The Mandalorian", Count: 50, Available: true, UserId: 2},
 	}
 
 	for _, toy := range toys {
