a823a2755b2b4686fdeeb5b4457934bdc2b240a2
Ashton <ashtonhowa@gmail.com>
2025-02-01 14:21:56 -0500

    fixed logout route

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8045',
+        target: 'http://127.0.0.1:8047',
         changeOrigin: true,
         secure: false,
       },
