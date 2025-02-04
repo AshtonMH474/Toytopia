a1ea456cb3bdaef75f7f71aa6054736486d0685b
Ashton <ashtonhowa@gmail.com>
2025-02-03 23:29:16 -0500

    start of side bar

--- frontend/src/components/Nav/nav.tsx
+++ frontend/src/components/Nav/nav.tsx
@@ -1,27 +1,30 @@
 
-import { useSelector } from 'react-redux';
-import { IoMenu } from "react-icons/io5";
-import { IoIosExit } from "react-icons/io";
+// import { useSelector } from 'react-redux';
+// import { IoMenu } from "react-icons/io5";
+// import { IoIosExit } from "react-icons/io";
 import dino from '../../../images/dino.png'
 import { CgProfile } from "react-icons/cg";
 import { MdToys } from "react-icons/md";
 import { MdDashboardCustomize } from "react-icons/md";
 import { MdReviews } from "react-icons/md";
 import { FaHome } from "react-icons/fa";
-import ProfileButton from './ProfileButton';
+// import ProfileButton from './ProfileButton';
 
-import { RootState } from '../../store/store';
-import { User } from '../../store/session';
+// import { RootState } from '../../store/store';
+// import { User } from '../../store/session';
 import { Link } from 'react-router-dom';
 import './nav.css';
 
 
 function Navigation({isLoaded}) {
-    const sessionUser = useSelector<RootState, User | null>((state) => state.user.user);
+    // const sessionUser = useSelector<RootState, User | null>((state) => state.user.user);
 
 
     return (
+        <>
+        {isLoaded && (
         <div className='sidebar'>
+
             <div className='logoContainer'>
                 <img src={dino} alt="dino" className='dinoLogo'/>
                 <h2 className='title'>Toytopia</h2>
@@ -66,7 +69,10 @@ function Navigation({isLoaded}) {
                     </li>
                 </ul>
             </div>
+
         </div>
+        )}
+        </>
     )
 //   return (
 //     <div className='containerNav'>
