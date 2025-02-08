b35915400a15a38d46a7ad27b014453bd64a5f1f
Ashton <ashtonhowa@gmail.com>
2025-02-07 23:07:10 -0500

    start to making it so images are part of toys

--- backend/routes/images.go
+++ backend/routes/images.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -22,9 +23,6 @@ var (
 	bucket = "toytopiaimages"
 	region = "us-east-2"
 )
-var body struct {
-	ImageURL string `json:"image_url"`
-}
 
 type ToyImageSerial struct {
 	// not model Toy, see this as serialzer
@@ -36,6 +34,15 @@ type ToyImageSerial struct {
 
 func CreateResImage(image models.ToyImage, toy ToySerialNoUser) ToyImageSerial {
 	return ToyImageSerial{ID: image.ID, PrimaryImg: image.PrimaryImg, ImgUrl: image.ImgUrl, Toy: toy}
+}
+func FindImagesByToyId(id int, images *[]models.ToyImage) error {
+	query := database.Database.Db.Model(&models.ToyImage{})
+	query = query.Where("toy_id == ?", id)
+	if err := query.Find(&images).Error; err != nil {
+		return errors.New("images do not exist")
+	}
+	return nil
+
 }
 
 func initS3Session() *s3.S3 {
@@ -51,6 +58,7 @@ func initS3Session() *s3.S3 {
 }
 
 func CreateToyImage(c *fiber.Ctx) error {
+	// checks for authorziation
 	tokenString := c.Get("Authorization")
 	if tokenString == "" {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
@@ -71,30 +79,33 @@ func CreateToyImage(c *fiber.Ctx) error {
 	if err := findUser(int(tokenUserID), &user); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
+	// checks to see if id is an int
 	id, err := c.ParamsInt("id")
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Please ensure the ID is an integer",
 		})
 	}
+	// finds toy based off int
 	var toy models.Toy
 	if err := findToy(id, &toy); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
-
+	// if toyid and userid are not same cant create img
 	if toy.UserId != int(user.ID) {
 		return c.Status(401).JSON(fiber.Map{
 			"error": "Unathorized",
 		})
 	}
-
+	// parses body
 	var image models.ToyImage
 	if err := c.BodyParser(&image); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
-
+	// toyId becomes toy id
 	image.ToyId = int(id)
 
+	// validates info
 	validationErrors := make(map[string]string)
 	if len(image.ImgUrl) < 1 {
 		validationErrors["imgUrl"] = "Img url required"
@@ -104,6 +115,7 @@ func CreateToyImage(c *fiber.Ctx) error {
 		return c.Status(400).JSON(fiber.Map{"errors": validationErrors})
 	}
 
+	// grabs image
 	resp, err := http.Get(image.ImgUrl)
 
 	if err != nil {
@@ -140,60 +152,10 @@ func CreateToyImage(c *fiber.Ctx) error {
 	imageURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileName)
 	// the image new url is this one
 	image.ImgUrl = imageURL
-
+	// creates image and returns it
 	database.Database.Db.Create(&image)
 	resToy := NoUserResToy(toy)
 	resImage := CreateResImage(image, resToy)
 	return c.Status(201).JSON(resImage)
 
-}
-
-func UploadImageFromURL(c *fiber.Ctx) error {
-	// Get the image URL from the request body (JSON)
-
-	if err := c.BodyParser(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Failed to parse request body", "error": err.Error()})
-	}
-
-	// Fetch the image from the URL
-	resp, err := http.Get(body.ImageURL)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to fetch image", "error": err.Error()})
-	}
-	defer resp.Body.Close()
-
-	// Read the image content into memory
-	imageData, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to read image content", "error": err.Error()})
-	}
-
-	// Create a buffer to store the image content
-	imageBuffer := bytes.NewReader(imageData)
-
-	// Generate a unique file name
-	fileName := fmt.Sprintf("image_%d.jpg", time.Now().Unix())
-
-	// Initialize AWS S3 client
-	s3Client := initS3Session()
-
-	// Upload the image to S3
-	_, err = s3Client.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(fileName),
-		Body:   imageBuffer,
-		ACL:    aws.String("public-read"),
-	})
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to upload image", "error": err.Error()})
-	}
-
-	// Construct the S3 URL for the uploaded image
-	imageURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileName)
-
-	// Return the image URL in the response
-	return c.Status(201).JSON(fiber.Map{
-		"message": "Image uploaded successfully",
-		"url":     imageURL,
-	})
 }
