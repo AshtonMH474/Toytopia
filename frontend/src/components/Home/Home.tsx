b9c2aac3249d4561587979fa12a69f808ab7941d
Ashton <ashtonhowa@gmail.com>
2025-02-08 23:13:19 -0500

    signup works well

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,19 +1,31 @@
 
-import girlWithToys from '../../../images/girl_with_toys.webp';
+import { useModal } from '../../Context/Modal';
+import SignupFormModal from '../SignupFormModal/SignupFormModal';
 import './Home.css';
 
 function Home() {
+    const {setModalContent} = useModal()
+
+    const signup = () => {
+        setModalContent(<SignupFormModal />);
+    }
+
     return (
         <div className='home'>
             <div className='home images'>
                 <div className='image1'>
-                    {/* Text on top of the background image */}
                     <div className='text-overlay'>
                        Shop Your Favorite Toys
                        <button className='seeall'>See All</button>
                     </div>
                 </div>
-                <img className='image2' src={girlWithToys} alt="kids" />
+                <div className='image2'>
+                <div className='text-overlay overlay2'>
+                        Join Us Now
+                       <button onClick={signup} className='seeall'>Sign Up</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
