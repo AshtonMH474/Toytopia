cece8cc7ab3be48d5f1e48d64e775d225ecad023
Ashton <ashtonhowa@gmail.com>
2025-02-17 19:14:03 -0500

    trying to fix filter bug for postgres

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => ({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8062',
+        target: 'http://127.0.0.1:8064',
         changeOrigin: true,
         secure: false,
       },
